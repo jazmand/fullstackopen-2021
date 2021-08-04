cf6f7bc942315afb191e60c62fb88d16ca9a4e27
jazmand <atothezmand@gmail.com>
2021-08-04 18:46:21 +0900

    complete 4.6 and 4.7

--- part4/blog/tests/total_likes.test.js
+++ part4/blog/tests/total_likes.test.js
@@ -1,12 +1,5 @@
 const listHelper = require('../utils/list_helper');
 
-test('dummy returns one', () => {
-	const blogs = [];
-
-	const result = listHelper.dummy(blogs);
-	expect(result).toBe(1);
-});
-
 describe('Total Likes', () => {
 	const listWithNoBlogs = [];
 	test('of empty list is zero', () => {
@@ -74,58 +67,5 @@ describe('Total Likes', () => {
 	test('of a bigger list is calculated right', () => {
 		result = listHelper.totalLikes(listWithMultipleBlogs);
 		expect(result).toBe(29);
-	});
-});
-
-describe('Favorite Blog', () => {
-	const listWithMultipleBlogs = [
-		{
-			_id: '5a422aa71b54a676234d17f8',
-			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
-			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-			likes: 5,
-			__v: 0,
-		},
-		{
-			_id: '5a422b3a1b54a676234d17f9',
-			title: 'Canonical string reduction',
-			author: 'Edsger W. Dijkstra',
-			url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-			likes: 12,
-			__v: 0,
-		},
-		{
-			_id: '5a422b891b54a676234d17fa',
-			title: 'First class tests',
-			author: 'Robert C. Martin',
-			url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-			likes: 10,
-			__v: 0,
-		},
-		{
-			_id: '5a422ba71b54a676234d17fb',
-			title: 'TDD harms architecture',
-			author: 'Robert C. Martin',
-			url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-			likes: 0,
-			__v: 0,
-		},
-		{
-			_id: '5a422bc61b54a676234d17fc',
-			title: 'Type wars',
-			author: 'Robert C. Martin',
-			url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-			likes: 2,
-			__v: 0,
-		},
-	];
-	test('when most liked blog is found correctly', () => {
-		result = listHelper.favoriteBlog(listWithMultipleBlogs);
-		expect(result).toEqual({
-			title: 'Canonical string reduction',
-			author: 'Edsger W. Dijkstra',
-			likes: 12,
-		});
 	});
 });
