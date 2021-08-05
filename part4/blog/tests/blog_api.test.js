9fbc8a23d013e3c60fd454184d43a318e6eae373
jazmand <atothezmand@gmail.com>
2021-08-05 17:27:38 +0900

    complete prep files before async/await section

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const Blog = require('../models/blog');
+const initialBlogs = [
+	{
+		title: 'First blog post',
+		author: 'Judi',
+		url: 'www.blog.com',
+	},
+	{
+		title: 'Second blog post',
+		author: 'Yukiko',
+		url: 'www.secondblog.com',
+	},
+];
+beforeEach(async () => {
+	await Blog.deleteMany({});
+	let blogObject = new Blog(initialBlogs[0]);
+	await blogObject.save();
+	blogObject = new Blog(initialBlogs[1]);
+	await blogObject.save();
+});
+
+test('all blogs are returned as json', async () => {
+	const response = await api.get('/api/blogs');
+
+	expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test('a specific blog is within the returned blogs', async () => {
+	const response = await api.get('/api/blogs');
+
+	const titles = response.body.map((r) => r.title);
+	expect(titles).toContain('Second blog post');
+});
+
+afterAll(() => {
+	mongoose.connection.close();
+});
