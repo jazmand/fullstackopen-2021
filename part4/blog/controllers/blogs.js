620fc3748986ead825810e334a3b5d9546eaaa4a
jazmand <atothezmand@gmail.com>
2021-08-06 17:47:23 +0900

    add functionality for deleting blog posts

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -16,20 +16,13 @@ blogsRouter.post('/', async (request, response) => {
 	response.status(201).json(savedBlog);
 });
 
-// With promises:
-
-// blogsRouter.get('/', (request, response) => {
-// 	Blog.find({}).then((blogs) => {
-// 		response.json(blogs);
-// 	});
-// });
-
-// blogsRouter.post('/', (request, response) => {
-// 	const blog = new Blog(request.body);
-
-// 	blog.save().then((result) => {
-// 		response.status(201).json(result);
-// 	});
-// });
+blogsRouter.delete('/:id', async (request, response, next) => {
+	try {
+		await Blog.findByIdAndRemove(request.params.id);
+		response.status(204).end();
+	} catch (exception) {
+		next(exception);
+	}
+});
 
 module.exports = blogsRouter;
