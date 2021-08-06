d44c4a25cd2b00a21434819b365a5e970681e7d9
jazmand <atothezmand@gmail.com>
2021-08-06 19:35:25 +0900

    Eliminate try-catch with express-async-errors library

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -17,27 +17,19 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-	try {
-		await Blog.findByIdAndRemove(request.params.id);
-		response.status(204).end();
-	} catch (exception) {
-		next(exception);
-	}
+	await Blog.findByIdAndRemove(request.params.id);
+	response.status(204).end();
 });
 
 blogsRouter.put('/:id', async (request, response, next) => {
-	try {
-		const body = request.body;
-		const blog = {likes: body.likes};
+	const body = request.body;
+	const blog = {likes: body.likes};
 
-		const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
-			new: true,
-		});
-		response.json(updatedBlog);
-		response.status(200).end();
-	} catch (exception) {
-		next(exception);
-	}
+	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+		new: true,
+	});
+	response.json(updatedBlog);
+	response.status(200).end();
 });
 
 module.exports = blogsRouter;
