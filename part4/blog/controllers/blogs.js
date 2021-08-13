2e601cfe68f46e6a308ce89207bbb48387d7a633
jazmand <atothezmand@gmail.com>
2021-08-13 20:59:42 +0900

    complete 4.21

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -11,8 +11,8 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
 	const body = request.body;
-
 	const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
 	if (!request.token || !decodedToken.id) {
 		return response.status(401).json({error: 'token missing or invalid'});
 	}
@@ -36,8 +36,22 @@ blogsRouter.post('/', async (request, response, next) => {
 });
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-	await Blog.findByIdAndRemove(request.params.id);
-	response.status(204).end();
+	const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+	if (!request.token || !decodedToken.id) {
+		return response.status(401).json({error: 'token missing or invalid'});
+	}
+
+	const userid = request.params.id;
+	const blog = await Blog.findById(userid);
+	console.log(`first: ${userid.toString()} second: ${blog.user.toString()}`);
+	if (blog.user.toString() === userid.toString()) {
+		await Blog.findByIdAndRemove(userId);
+		response.status(204).end();
+	}
+	return response.status(401).json({
+		error: 'Blogs can only be deleted by the user who added the blog',
+	});
 });
 
 blogsRouter.put('/:id', async (request, response, next) => {
