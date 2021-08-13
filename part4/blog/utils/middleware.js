be88bb115ccd8ff7547573a8e18fd4227a99e73b
jazmand <atothezmand@gmail.com>
2021-08-13 20:23:51 +0900

    complete 4.20

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -31,8 +31,19 @@ const errorHandler = (error, request, response, next) => {
 	next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+	const authorization = request.get('authorization');
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		request.token = authorization.substring(7);
+		return next();
+	}
+	request.token = null;
+	return next();
+};
+
 module.exports = {
 	requestLogger,
 	unknownEndpoint,
 	errorHandler,
+	tokenExtractor,
 };
