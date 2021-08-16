952d83ddf3741ec3e503391ec7b73a7e29de3f6a
jazmand <atothezmand@gmail.com>
2021-08-16 18:17:29 +0900

    add toggable blogs with styling

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,26 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react';
+import Togglable from './Togglable';
 
-export default Blog
+const Blog = ({blog}) => {
+	const blogStyle = {
+		paddingTop: '10px',
+		paddingLeft: '2px',
+		border: 'solid',
+		borderWidth: '1px',
+		marginBottom: '5px',
+	};
+	return (
+		<div style={blogStyle}>
+			{blog.title}
+			<Togglable buttonLabel='view'>
+				<div>{blog.url}</div>
+				<div>
+					{`likes ${blog.likes}`} <button>like</button>
+				</div>
+				<div>{blog.author}</div>
+			</Togglable>
+		</div>
+	);
+};
+
+export default Blog;
