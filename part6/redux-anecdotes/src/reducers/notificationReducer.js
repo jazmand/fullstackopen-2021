43d4c49f18a9aeee792e43625c3eddc45a6239dc
jazmand <atothezmand@gmail.com>
2021-08-25 17:11:04 +0900

    complete 6.10

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,32 @@
+const notificationReducer = (state = null, action) => {
+	switch (action.type) {
+		case 'SHOW_NOTIFICATION': {
+			return action.data.notification;
+		}
+		case 'HIDE_NOTIFICATION': {
+			return action.data.notification;
+		}
+		default:
+			return state;
+	}
+};
+
+export const showNotification = (notification) => {
+	return {
+		type: 'SHOW_NOTIFICATION',
+		data: {
+			notification,
+		},
+	};
+};
+
+export const hideNotification = () => {
+	return {
+		type: 'HIDE_NOTIFICATION',
+		data: {
+			notification: null,
+		},
+	};
+};
+
+export default notificationReducer;
