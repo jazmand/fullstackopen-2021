eacfb95de2847118ae8c1d9f91fac58752dd836b
jazmand <atothezmand@gmail.com>
2021-08-25 16:07:21 +0900

    complete 6.9

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,7 +19,7 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'VOTE': {
 			return state.map((anecdote) =>
@@ -56,4 +56,4 @@ export const createAnecdote = (content) => {
 	};
 };
 
-export default reducer;
+export default anecdoteReducer;
